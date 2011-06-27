c04f63347e70b76c1dc47ca996cd4c528cdc90fe
Todd Owen <todd.m.owen@gmail.com>
2011-06-27 21:14:22 +0800

    Add command composition via the Commands.apply method. Tests pass!

--- scala/function-composition/rovers.scala
+++ scala/function-composition/rovers.scala
@@ -19,15 +19,20 @@ case class Rover(x: Int, y: Int, d: Direction)
 
 
 object Commands {
-	val L = (r: Rover) => r match {
+	type Command = (Rover) => Rover
+
+	// Compose a list of Commands into a new Command
+	def apply(commands: Command*) = commands.reduceLeft(_ andThen _)
+
+	val L: Command = _ match {
 		case Rover(x, y, d) => Rover(x, y, left(d))
 	}
 
-	val R = (r: Rover) => r match {
+	val R: Command = _ match {
 		case Rover(x, y, d) => Rover(x, y, right(d))
 	}
 
-	val M = (r: Rover) => r match {
+	val M: Command = _ match {
 		case Rover(x, y, d) => unitVector(d) match {
 			case (dx, dy) => Rover(x + dx, y + dy, d)
 		}
@@ -37,9 +42,9 @@ import Commands.{L, R, M}
 
 
 val testRover1 = Rover(1, 2, N)
-//val testCommands1 = Commands(L, M, L, M, L, M, L, M, M)
-//assert (Rover(1, 3, N) == testCommands1(testRover1))
+val testCommands1 = Commands(L, M, L, M, L, M, L, M, M)
+assert (Rover(1, 3, N) == testCommands1(testRover1))
 
 val testRover2 = Rover(3, 3, E)
-//val testCommands2 = Commands(M, M, R, M, M, R, M, R, R, M)
-//assert (Rover(5, 1, E) == testCommands2(testRover2))
+val testCommands2 = Commands(M, M, R, M, M, R, M, R, R, M)
+assert (Rover(5, 1, E) == testCommands2(testRover2))
