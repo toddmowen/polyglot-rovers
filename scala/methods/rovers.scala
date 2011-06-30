87c1d2fe6e9d2c5f3cf34a96ab1315416d6fb662
Todd Owen <todd.m.owen@gmail.com>
2011-07-01 06:23:36 +0800

    Add another Scala solution, "methods".

---
+++ scala/methods/rovers.scala
@@ -0,0 +1,37 @@
+object Directions {
+	class Direction(val dx: Int, val dy: Int) {
+		// Rotation is modelled as modulo arithmetic over the
+		// the domain defined by the "ordered" array.
+		def +(i: Int) = {
+			val positive = i % ordered.length + ordered.length
+			val thisIndex = ordered.indexOf(this)
+			ordered((thisIndex + positive) % ordered.length)
+		}
+		def -(i: Int) = this + (-i)
+	}
+
+	// Defining constants as case objects theoretically enables pattern
+	// matching on them, although we don't use that in this solution.
+	// Additionally, a toString method is automatically generated.
+	case object N extends Direction(0, 1)
+	case object E extends Direction(1, 0)
+	case object S extends Direction(0, -1)
+	case object W extends Direction(-1, 0)
+
+	val ordered = Vector[Direction](N, E, S, W)
+}
+import Directions.{Direction, N, E, S, W}
+
+
+case class Rover(x: Int, y: Int, d: Direction) {
+	def L = new Rover(x, y, d - 1)
+	def R = new Rover(x, y, d + 1)
+	def M = new Rover(x + d.dx, y + d.dy, d)
+}
+
+
+val testRover1 = Rover(1, 2, N)
+assert (Rover(1, 3, N) == testRover1.L.M.L.M.L.M.L.M.M)
+
+val testRover2 = Rover(3, 3, E)
+assert (Rover(5, 1, E) == testRover2.M.M.R.M.M.R.M.R.R.M)
