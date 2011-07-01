0ed5dbdf25c0660c8d4f0312c86b9eb9db3b4526
Todd Owen <todd.m.owen@gmail.com>
2011-07-01 22:11:15 +0800

    Slight change to tests to separate "input" line from "output" line.

--- scala/methods/test.scala
+++ scala/methods/test.scala
@@ -3,11 +3,11 @@ import rovers.Rover
 
 object RoverTest {
 	def main(args: Array[String]) {
-		val testRover1 = Rover(1, 2, N)
-		assert (Rover(1, 3, N) == testRover1.L.M.L.M.L.M.L.M.M)
+		val result1 = Rover(1, 2, N).L.M.L.M.L.M.L.M.M
+		assert (Rover(1, 3, N) == result1)
 
-		val testRover2 = Rover(3, 3, E)
-		assert (Rover(5, 1, E) == testRover2.M.M.R.M.M.R.M.R.R.M)
+		val result2 = Rover(3, 3, E).M.M.R.M.M.R.M.R.R.M
+		assert (Rover(5, 1, E) == result2)
 
 		println("Tests pass!")
 	}
