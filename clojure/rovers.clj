0df3e9261d59cf458817f70f1b8e6fab465fc8d5
Todd Owen <todd.m.owen@gmail.com>
2011-10-25 22:23:00 +0800

    Prettier printing of bearings and commands (instead of a mangled class name such as "rovers$N").

--- clojure/rovers.clj
+++ clojure/rovers.clj
@@ -1,7 +1,23 @@
-(defn N [vec] (update-in vec [1] (partial + 1)))
-(defn E [vec] (update-in vec [0] (partial + 1)))
-(defn S [vec] (update-in vec [1] (partial + -1)))
-(defn W [vec] (update-in vec [0] (partial + -1)))
+(ns rovers)
+
+;; equivalent to defn, but add name as metadata to the fn
+(defmacro defn-named [name & decls]
+  (list `def name (list `with-meta (cons `fn decls) {:name (str name)})))
+
+;; print-method that uses metadata from defn-named
+(let
+  [old-print-method (get-method print-method clojure.lang.Fn)]
+  (defmethod print-method clojure.lang.Fn [o, ^java.io.Writer w]
+    (let
+      [name (:name (meta o))]
+      (if name
+        (.write w name)
+        (old-print-method o w)))))
+
+(defn-named N [vec] (update-in vec [1] (partial + 1)))
+(defn-named E [vec] (update-in vec [0] (partial + 1)))
+(defn-named S [vec] (update-in vec [1] (partial + -1)))
+(defn-named W [vec] (update-in vec [0] (partial + -1)))
 
 (defn turn [quarters bearing]
   (nth
@@ -10,9 +26,9 @@
 
 (defrecord Rover [position bearing])
 
-(defn M [rover] (update-in rover [:position] (:bearing rover)))
-(defn R [rover] (update-in rover [:bearing] (partial turn 1)))
-(defn L [rover] (update-in rover [:bearing] (partial turn -1)))
+(defn-named M [rover] (update-in rover [:position] (:bearing rover)))
+(defn-named R [rover] (update-in rover [:bearing] (partial turn 1)))
+(defn-named L [rover] (update-in rover [:bearing] (partial turn -1)))
 
 (defn command [rover & cmds] (reduce #(%2 %1) rover cmds))
 
