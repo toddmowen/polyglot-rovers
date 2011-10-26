9b580bf531525445f78e9d95f425b126b10fd00b
Todd Owen <todd.m.owen@gmail.com>
2011-10-26 23:49:39 +0800

    Give Rover type the same :x and :y interface that vectors have. The bearing functions and the command functions are now interchangeable!

--- clojure/rovers.clj
+++ clojure/rovers.clj
@@ -14,19 +14,19 @@
         (.write w name)
         (old-print-method o w)))))
 
-(defn-named N [vec] (update-in vec [:y] inc))
-(defn-named E [vec] (update-in vec [:x] inc))
-(defn-named S [vec] (update-in vec [:y] dec))
-(defn-named W [vec] (update-in vec [:x] dec))
+(defn-named N [rover] (update-in rover [:y] inc))
+(defn-named E [rover] (update-in rover [:x] inc))
+(defn-named S [rover] (update-in rover [:y] dec))
+(defn-named W [rover] (update-in rover [:x] dec))
 
 (defn turn [quarters bearing]
   (nth
     (drop-while (partial not= bearing) (list N E S W N E S))
     (mod quarters 4)))    
 
-(defrecord Rover [position bearing])
+(defrecord Rover [x y bearing])
 
-(defn-named M [rover] (update-in rover [:position] (:bearing rover)))
+(defn-named M [rover] ((:bearing rover) rover))
 (defn-named R [rover] (update-in rover [:bearing] (partial turn 1)))
 (defn-named L [rover] (update-in rover [:bearing] (partial turn -1)))
 
@@ -34,12 +34,12 @@
 
 ;; sample data
 (assert (=
-  (Rover. {:x 1 :y 3} N)
+  (Rover. 1 3 N)
   (command
-    (Rover. {:x 1 :y 2} N)
+    (Rover. 1 2 N)
     L M L M L M L M M)))
 (assert (=
-  (Rover. {:x 5 :y 1} E)
+  (Rover. 5 1 E)
   (command
-    (Rover. {:x 3 :y 3} E)
+    (Rover. 3 3 E)
     M M R M M R M R R M)))
