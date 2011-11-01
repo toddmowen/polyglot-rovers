072e5a3432e154c4704922d1b13310c12ac332d5
Todd Owen <todd.m.owen@gmail.com>
2011-11-01 22:10:17 +0800

    Define directions and commands.

--- clojure/sequences/rovers.clj
+++ clojure/sequences/rovers.clj
@@ -1,5 +1,20 @@
 (ns sequences.rovers)
 
+;; rover state is represented by a vector, [pos dir]
+;; where: pos is the current position, [x y]
+;;        dir is the current heading as a unit vector, [x y]
+
+(defn quarter-turn [[x y]] [y (- x)])
+
+;; define compass directions in terms of quarter-turn
+(def directions
+  (zipmap [:N :E :S :W] (iterate quarter-turn [0 1])))
+
+(def commands
+  {:R (fn [[pos dir]] [pos (quarter-turn dir)])
+   :L (fn [[pos dir]] [pos (quarter-turn (quarter-turn (quarter-turn dir)))])
+   :M (fn [[pos dir]] [(map + pos dir) dir])})
+
 (defn tokenize [line]
   (map second (re-seq #"\s*([0-9]+|\w)" line)))
 
