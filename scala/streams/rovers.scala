ee4449f78c3cebe46ffc0cdd10e4f4d4f23b8c03
Todd Owen <todd.m.owen@gmail.com>
2011-11-12 18:42:10 +0800

    Add Rover.execute method for applying a string of commands.

--- scala/streams/rovers.scala
+++ scala/streams/rovers.scala
@@ -7,6 +7,12 @@ case class Vector(x: Int, y: Int) {
 
 case class Rover(pos: Vector, dir: Vector) {
 	override def toString = pos.x + " " + pos.y + " " + Rover.directionToName(dir)
+
+	// implicit conversion allows parameter to be an ordinary String too
+	def execute(cmds: Seq[Char]): Rover = cmds match {
+		case Nil => this
+		case Seq(cmd, rest @ _*) => Rover.commands(cmd)(this) execute rest
+	}
 }
 
 object Rover {
@@ -16,8 +22,22 @@ object Rover {
 	val directionToName = directions map (_.swap)
 
 	val commands = Map(
-		"R" -> ((r: Rover) => Rover(r.pos, r.dir.quarterTurn)),
-		"L" -> ((r: Rover) => Rover(r.pos, r.dir.quarterTurn.quarterTurn.quarterTurn)),
-		"M" -> ((r: Rover) => Rover(r.pos + r.dir, r.dir))
+		'R' -> ((r: Rover) => Rover(r.pos, r.dir.quarterTurn)),
+		'L' -> ((r: Rover) => Rover(r.pos, r.dir.quarterTurn.quarterTurn.quarterTurn)),
+		'M' -> ((r: Rover) => Rover(r.pos + r.dir, r.dir))
 	)
+
+	private val roverRegex = """([0-9]+)\s+([0-9]+)\s+(\w)""".r
+	def apply(s: String): Rover = {
+		val roverRegex(xstr, ystr, dirstr) = s
+		Rover(Vector(xstr.toInt, ystr.toInt), directions(dirstr))
+	}
+}
+
+object Test {
+	def main(args: Array[String]) = {
+		assert("1 3 N" == (Rover("1 2 N") execute "LMLMLMLMM" toString))
+		assert("5 1 E" == (Rover("3 3 E") execute "MMRMMRMRRM" toString))
+		println("Tests pass")
+	}
 }
