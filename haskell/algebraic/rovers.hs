3a1aba4b3d36415ded1907c761cdfcac5de6728a
Todd Owen <todd.m.owen@gmail.com>
2012-04-10 21:39:44 +0800

    Introduce a type class, so we can execute lists of Commands.

--- haskell/algebraic/rovers.hs
+++ haskell/algebraic/rovers.hs
@@ -1,3 +1,6 @@
+import Data.List (foldl')
+
+
 data Bearing = N | E | S | W
     deriving (Show, Eq, Enum, Bounded)
 
@@ -12,15 +15,26 @@ left, right :: Bearing -> Bearing
 left b  = if (b == minBound) then maxBound else pred b
 right b = if (b == maxBound) then minBound else succ b
 
-exec (Rover x y bearing) cmd =
-    case cmd of
-        L -> Rover x y (left bearing)
-        R -> Rover x y (right bearing)
-        M -> case bearing of
-            E -> Rover (x+1) y bearing
-            N -> Rover x (y+1) bearing
-            W -> Rover (x-1) y bearing
-            S -> Rover x (y-1) bearing
 
-output1 = (Rover 1 2 N) `exec` L `exec` M `exec` L `exec` M `exec` L `exec` M
-                        `exec` L `exec` M `exec` M
+class Program a where
+    exec :: Rover -> a -> Rover
+
+instance Program Command where
+    exec (Rover x y bearing) cmd =
+        case cmd of
+            L -> Rover x y (left bearing)
+            R -> Rover x y (right bearing)
+            M -> case bearing of
+                E -> Rover (x+1) y bearing
+                N -> Rover x (y+1) bearing
+                W -> Rover (x-1) y bearing
+                S -> Rover x (y-1) bearing
+
+instance (Program a) => Program [a] where
+    exec = foldl' exec
+
+
+-- sample inputs from problem spec
+output1 = (Rover 1 2 N) `exec` [L,M,L,M,L,M,L,M,M]
+output2 = (Rover 3 3 E) `exec` [[M,M,R,M],[M,R,M],[R,R,M]]
+
