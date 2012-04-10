beb9ead39bd1080ce182eb4722a730a00a87c4c9
Todd Owen <todd.m.owen@gmail.com>
2012-04-10 21:22:21 +0800

    Add the central 'exec' function.

--- haskell/algebraic/rovers.hs
+++ haskell/algebraic/rovers.hs
@@ -1,7 +1,10 @@
 data Bearing = N | E | S | W
     deriving (Show, Eq, Enum, Bounded)
 
-data Rover = Rover Int Int Bearing
+data Position = Position Int Int
+    deriving (Show, Eq)
+
+data Rover = Rover Position Bearing
     deriving (Show, Eq)
 
 data Command = L | R | M
@@ -11,3 +14,17 @@ data Command = L | R | M
 left, right :: Bearing -> Bearing
 left b  = if (b == minBound) then maxBound else pred b
 right b = if (b == maxBound) then minBound else succ b
+
+move :: Int -> Int -> Position -> Position
+move dx dy (Position x y) = Position (x+dx) (y+dy)
+
+exec :: Rover -> Command -> Rover
+exec (Rover pos bearing) cmd =
+    case cmd of
+        L -> Rover pos (left bearing)
+        R -> Rover pos (right bearing)
+        M -> case bearing of
+            E -> Rover (move   1   0  pos) bearing
+            N -> Rover (move   0   1  pos) bearing
+            W -> Rover (move (-1)  0  pos) bearing
+            S -> Rover (move   0 (-1) pos) bearing
