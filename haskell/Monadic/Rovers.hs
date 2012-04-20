7b89e5d899e51a98054e0991f6b672b08c388463
Todd Owen <todd.m.owen@gmail.com>
2012-04-20 23:29:28 +0800

    Initial commit of Monadic.Rovers

---
+++ haskell/Monadic/Rovers.hs
@@ -0,0 +1,28 @@
+module Monadic.Rovers where
+
+import Control.Monad.State
+
+data Rover = Rover { x::Int, y::Int, bearing::Char }
+    deriving (Show, Eq)
+
+type RoverState = State Rover
+
+
+-- rover commands
+
+r,l,m :: RoverState ()
+r = modify $ \rover -> rover { bearing = bearing rover `successorIn` "NESWN" }
+l = modify $ \rover -> rover { bearing = bearing rover `successorIn` "NWSEN" }
+m = modify $ \rover ->
+    case bearing rover of
+        'N' -> rover { y = y rover + 1 }
+        'E' -> rover { x = x rover + 1 }
+        'S' -> rover { y = y rover - 1 }
+        'W' -> rover { x = x rover - 1 }
+
+x `successorIn` xs = let (_,it:successor:_) = break (==x) xs in successor 
+
+
+-- sample inputs from problem spec
+
+output1 = execState (l >> m >> l >> m >> l >> m >> l >> m >> m) (Rover 1 2 'N')
