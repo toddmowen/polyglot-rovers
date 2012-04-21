f5304f427fd7712028fc39af82529688e19fb027
Todd Owen <todd.m.owen@gmail.com>
2012-04-21 08:31:51 +0800

    Add monadic implementation to test script.

--- haskell/tests.hs
+++ haskell/tests.hs
@@ -4,28 +4,37 @@ import Test.QuickCheck
 
 import Algebraic.Rovers
 import Vector.Rovers
+import Monadic.Rovers
 
 
 prop_implementationsEquivalent x y =
     forAll bearingChar $ \bearing ->
     forAll commandString $ \cmds ->
-        (algebraicResult x y bearing cmds) == (vectorResult x y bearing cmds)
+        let algebraicResult = runAlgebraicRovers x y bearing cmds
+            vectorResult = runVectorRovers x y bearing cmds
+            monadicResult = runMonadicRovers x y bearing cmds
+        in algebraicResult == vectorResult && algebraicResult == monadicResult
   where
     bearingChar = elements ['N', 'S', 'E', 'W']
     commandString = listOf (elements ['L', 'R', 'M'])
 
 
-algebraicResult x y bearing cmds = (x', y', show bearing')
+runAlgebraicRovers x y bearing cmds = (x', y', show bearing')
   where
     (Algebraic.Rovers.Rover x' y' bearing') =
         (Algebraic.Rovers.Rover x y (read [bearing])) `exec` cmds
 
-vectorResult x y bearing cmds = (x', y', bearing')
+runVectorRovers x y bearing cmds = (x', y', bearing')
   where
     (Vector.Rovers.Rover (x',y') b') = f (rover x y b)
     b = charToBearing bearing
     f = foldl (flip (.)) id $ map charToCommand cmds
     bearing' = bearingToUpchar b' : ""
+
+runMonadicRovers x y bearing cmds = (x', y', b':"")
+  where
+    (Monadic.Rovers.Rover x' y' b') =
+        runRover (Monadic.Rovers.Rover x y bearing) (commandsFromString cmds)
 
 
 main = quickCheck prop_implementationsEquivalent
