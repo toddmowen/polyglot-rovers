6c10297231315743e627bcd3de9ff297f76ba130
Todd Owen <todd.m.owen@gmail.com>
2012-04-29 08:30:45 +0800

    Add "tests" project to the solution.

---
+++ c++/externs/tests/tests.cpp
@@ -0,0 +1,13 @@
+/*
+ * Rather than introducing a dependency on NUnit or some other test framework,
+ * just use some simple assert-based tests.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+}
